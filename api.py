e34cc3f17befcee7834a1526a1d0c8e52c2b80ad
Vladimir Ivanov <vlad.v.ivanov@icloud.com>
2018-04-15 21:44:19 -0400

    #3 switched to flask-restful

--- api.py
+++ api.py
@@ -1,25 +1,33 @@
-import sys, traceback
+import flask
 from flask import Flask
 from flask import jsonify
-app = Flask(__name__)
-
+from flask_restful import Resource, Api
+import sys, traceback
 import notes
+import pandas as pd
 
-@app.route('/')
-def index():
-    return 'Welcome to open finance!'
+app = Flask(__name__)
+api = Api(app)
 
-@app.route('/note/<string:symbol>', methods=['GET'])
-def note(symbol):
-    try:
-        f = getattr(__import__("notes.%s" % (symbol), fromlist=['notes']), 'response')
-        r = f()
-        return r.to_json()
-    except ModuleNotFoundError:
-        app.logger.error("Module for %s not found." % symbol)
-        return "Pricing for %s not found" % symbol, 404
-    except :
-        m, e, t = sys.exc_info()
-        app.logger.error(traceback.print_tb(t))
-        app.logger.error(e)
-        return 'Internal error occurred.', 500
+class Index(Resource):
+    def get(self):
+        return {'message': 'Welcome to open finance!'}
+
+api.add_resource(Index, '/')
+
+class Note(Resource):
+    def get(self, symbol):
+        try:
+            f = getattr(__import__("notes.%s" % (symbol), fromlist=['notes']), 'response')
+            r = f()
+            # assert app.debug == False
+            return pd.Series.to_json(r.portfolio, date_format='iso')
+        except ModuleNotFoundError:
+            app.logger.error("Module for %s not found." % symbol)
+            return {'message': "Pricing for %s not found." % symbol}, 404
+
+api.add_resource(Note, '/notes/<symbol>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
