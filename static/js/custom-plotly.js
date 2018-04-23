5b5744398ebabf32bb6ad2898aa198dd07b8bf01
Vladimir Ivanov <vlad.v.ivanov@icloud.com>
2018-04-23 06:24:05 -0400

    initial check-in of plotly.js and index template

---
+++ static/js/custom-plotly.js
@@ -0,0 +1,9 @@
+var Plotly = require('plotly.js/lib/core');
+
+// Load in the trace types for pie, and choropleth
+Plotly.register([
+    require('plotly.js/lib/pie'),
+    require('plotly.js/lib/choropleth')
+]);
+
+module.exports = Plotly;
