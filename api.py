0ade8a29e4e5b15f3bd2e71013320a4f97488788
Vladimir Ivanov <vlad.v.ivanov@icloud.com>
2018-04-25 04:05:32 -0400

    implemented timeseries plotting of a single stuctured note

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
-import flask
-from flask import Flask, request, render_template, Response, jsonify
-import sys, traceback
-import notes
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import jsonify
+from flask import Response
 import pandas as pd
 
 app = Flask(__name__)
+
 
 def request_wants_json():
     best = request.accept_mimetypes \
@@ -13,13 +15,14 @@ def request_wants_json():
         request.accept_mimetypes[best] > \
         request.accept_mimetypes['text/html']
 
+
 @app.route('/', methods=['GET'])
 def index():
     if request_wants_json():
-        return jsonify({'endpoints': [ 
+        return jsonify({'endpoints': [
             {'/': 'Index'},
             {'/notes/<symbol>': 'Single structured note historical value'}
-            ]} )
+        ]})
     else:
         return '''
         <html>
@@ -35,18 +38,24 @@ def index():
         </html>
         '''
 
+
 @app.route('/notes/<string:symbol>', methods=['GET'])
 def note(symbol):
-       try:
-           f = getattr(__import__("notes.%s" % (symbol), fromlist=['notes']), 'response')
-           r = f()
-           if request_wants_json():
-                return pd.Series.to_json(r.portfolio, orient='split', date_format='iso')           
-           else:
-                pass
-       except ModuleNotFoundError:
-           app.logger.error("Module for %s not found." % symbol)
-           return {'message': "Pricing for %s not found." % symbol}, 404
+    try:
+        f = getattr(__import__("notes.%s" %
+                               (symbol), fromlist=['notes']), 'response')
+        r = f()
+        result = pd.Series.to_json(r.portfolio, orient='split', date_format='iso')
+        if request_wants_json():
+            response = Response(result, status=200, mimetype='application/json')
+        else:
+            response = render_template('timeseries.html', data=result)
+        
+        return response
+    except ModuleNotFoundError:
+        app.logger.error("Module for %s not found." % symbol)
+        return {'message': "Pricing for %s not found." % symbol}, 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- notes/jhn919.py
+++ notes/jhn919.py
@@ -1,18 +1,20 @@
 def response():
     import pandas as pd
-    import os
     from pandas_datareader import data
     import fix_yahoo_finance as yf
     yf.pdr_override()
     close = pd.DataFrame()
     pct_growth = pd.DataFrame()
-    tickers = ['BNS.TO', 'BAM-A.TO', 'CM.TO', 'MFC.TO', 'NA.TO', 'POW.TO', 'RY.TO', 'SLF.TO', 'TD.TO']
-    close,pct_growth = load_data(tickers,close,pct_growth,pd.datetime(2011,2,16),pd.datetime(2017,2,16))
+    tickers = ['BNS.TO', 'BAM-A.TO', 'CM.TO', 'MFC.TO',
+               'NA.TO', 'POW.TO', 'RY.TO', 'SLF.TO', 'TD.TO']
+    close, pct_growth = load_data(tickers, close, pct_growth, pd.datetime(
+        2011, 2, 16), pd.datetime(2017, 2, 16))
     # need a nice graph of
     return pct_growth[['portfolio']]
     # Final payout of $144.90
 
-def load_data(tickers,close,pct_growth,start_date,end_date):
+
+def load_data(tickers, close, pct_growth, start_date, end_date):
     import pandas as pd
     from pathlib import Path
     import os.path
@@ -23,8 +25,8 @@ def load_data(tickers,close,pct_growth,start_date,end_date):
     for symbol in tickers:
         csv_path = os.path.join(app_root, "../quotes/" + symbol + ".csv")
         if not os.path.exists(csv_path):
-            print ("Loading data from Yahoo/Google: " + symbol)
-            data_source='google'
+            print("Loading data from Yahoo/Google: " + symbol)
+            data_source = 'google'
             start_date = '1950-01-01'
             end_date = '2049-12-31'
             df = data.get_data_yahoo(symbol, start_date, end_date)
@@ -35,10 +37,10 @@ def load_data(tickers,close,pct_growth,start_date,end_date):
         csv_path = os.path.join(app_root, "../quotes/" + symbol + ".csv")
         p = Path(csv_path)
         content = []
-        df1 = pd.read_csv(p,index_col="Date",parse_dates=True)
+        df1 = pd.read_csv(p, index_col="Date", parse_dates=True)
         df1 = df1[['Close']]
-        df1.columns=[symbol]
-        close = pd.concat([close,df1],axis=1)
+        df1.columns = [symbol]
+        close = pd.concat([close, df1], axis=1)
 
     # restrict to proper dates
     filter1 = close.index >= start_date
@@ -56,6 +58,6 @@ def load_data(tickers,close,pct_growth,start_date,end_date):
     pct_growth['portfolio'] = pct_growth.mean(axis=1)
 
     # portfolio calculation is done 3 days prior to maturity
-    pct_growth.iloc[-3:,-1] = pct_growth.iloc[-4,-1]
+    pct_growth.iloc[-3:, -1] = pct_growth.iloc[-4, -1]
 
-    return close,pct_growth
+    return close, pct_growth
